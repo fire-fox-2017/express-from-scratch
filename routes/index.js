0917ab403900cebe05b3701cb303411f9218d305
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-04-17 18:44:39 +0700

    done

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,21 @@
 'use strict'
 
 const express = require('express')
+const db = require('../models');
 
 // let app = express()
 
 let router = express.Router();
 
 router.get('/', function (request, response) {
-	response.send("I Love You!")
+	// response.send("I Love You!")
+	
+	db.User.findAll()
+	.then ( users => {
+		response.render('index', {title: "Scratch Express", users: users })
+
+	})
+
 })
 
 // app.listen(3000)
--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 'use strict'
 
-const express = require('express')
+var express = require('express')
+var path = require('path');
+
+var bodyParser = require('body-parser');
+
+
+
+var index = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
-var index = require('./routes/index');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 
 /*
 let app = express()
@@ -15,6 +31,7 @@ app.get('/', function (request, response) {
 */
 
 app.use('/', index);
+app.use('/users', users);
 
 
 
