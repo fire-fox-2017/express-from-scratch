ebe7ac3b8598f6aca2cbe5967cb06a40f2867b7f
ridho0 <ridho230994@gmail.com>
2017-04-19 15:04:28 +0700

    udah crud

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,9 @@ routes.get('/', function (req, res) {
     res.render ('./index',{tampilkan:users, title:"hai"})
   })
 })
+routes.get('/edit', function (req, res) {
+    res.render ('./edit')
+})
 
 routes.get('/add',function(req,res,next) {
   db.User.findAll()
@@ -20,6 +23,39 @@ routes.post('/add',function(req,res,next) {
     {firstname: req.body.firstname, lastname:req.body.lastname, phone: req.body.phone}
   ).then(data => {
     res.redirect('/user')
+  })
+})
+
+routes.get('/edit/:id', (req, res, next) => {
+  let id = req.params.id
+  db.User.findById(id)
+    .then(user => {
+      res.render('./edit', {user: user})
+    })
+})
+routes.post('/edit/:id', (req, res, next) => {
+  let id = req.params.id
+  db.User.update({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    phone: req.body.phone,
+  },{
+    where: {
+      id: id
+    }
+  })
+    .then( () => {
+      res.redirect('/user')
+    })
+})
+
+routes.get("/delete/:id", function (req, res) {
+  db.User.destroy({
+    where: {id: req.params.id}
+  }).then( () => {
+    res.redirect("/user")
+  }).catch(err => {
+    res.send(err.message)
   })
 })
 
